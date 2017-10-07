d5f365b353d9385f7e3de4ba6c4a726fd49af0b3
CubanJew <yfedorov624@gmail.com>
2017-10-07 11:31:14 -0500

    Delete macro_compile_macro.bat

--- macro_compile_macro.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM This generates build time stamp comprising of YY/MM/DD + HH:MM format
-setlocal
-for /f "tokens=2,3,4 delims=/ " %%f in ('date /t') do set d=%%h%%f%%g
-for /f "tokens=1,2 delims=: " %%f in ('time /t') do set t=%%f%%g
-echo -D BUILD_TIME=%d:~2,8%%t%
-endlocal
